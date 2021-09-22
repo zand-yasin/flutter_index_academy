a24e9ce2531472a515170810b12ea359a08f9a89
zand-yasin <zand.kurdy@gmail.com>
2021-09-23 00:19:17 +0300

    add firebase auth

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:dartlearning/myapp.dart';
 import 'package:dartlearning/providers/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -45,24 +46,91 @@ class Login extends StatelessWidget {
             //         padding: EdgeInsets.all(20),
             //         color: Colors.blue,
             //         child: Text('submit'))),
-            ElevatedButton(
-                onPressed: () {
-                  if (formGlobalKey.currentState!.validate()) {
-                    final userPrvider =
-                        Provider.of<User>(context, listen: false);
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () async {
+                      if (formGlobalKey.currentState!.validate()) {
+                        if (await login()) {
+                          final userPrvider =
+                              Provider.of<UserPrv>(context, listen: false);
 
-                    userPrvider.addEmail(email.text);
+                          userPrvider.addEmail(email.text);
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) => MyApp()));
+                          // Navigator.push(context,
+                          //     MaterialPageRoute(builder: (_) => MyApp()));
+                        }
+                      } else
+                        print('unsuccssull');
+                    },
+                    child: Text("Sign in")),
+                ElevatedButton(
+                    onPressed: () async {
+                      if (formGlobalKey.currentState!.validate()) {
+                        if (await register()) {
+                          final userPrvider =
+                              Provider.of<UserPrv>(context, listen: false);
 
-                    Navigator.push(
-                        context, MaterialPageRoute(builder: (_) => MyApp()));
-                  } else
-                    print('unsuccssull');
-                },
-                child: Text("Submit"))
+                          userPrvider.addEmail(email.text);
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) => MyApp()));
+                          // Navigator.push(context,
+                          //     MaterialPageRoute(builder: (_) => MyApp()));
+                        }
+                      } else
+                        print('unsuccssull');
+                    },
+                    child: Text("Sign up")),
+              ],
+            )
           ],
         ),
       ),
     );
+  }
+
+  Future<bool> register() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: email.text, password: password.text);
+
+      return true;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+      return false;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  Future<bool> login() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: email.text, password: password.text);
+
+      return true;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+      return false;
+    }
   }
 
   bool isPasswordValid(String password) => password.length >= 6;
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:dartlearning/login.dart';
+import 'package:dartlearning/myapp.dart';
 import 'package:dartlearning/providers/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:provider/provider.dart';
@@ -7,6 +10,7 @@ import 'package:provider/provider.dart';
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await Firebase.initializeApp();
 
   runApp(EasyLocalization(
       saveLocale: true,
@@ -16,7 +20,7 @@ main() async {
         Locale("ar", "SA"),
       ],
       child: ChangeNotifierProvider(
-        create: (context) => User(),
+        create: (context) => UserPrv(),
         child: App(),
       )));
 }
@@ -26,6 +30,13 @@ class App extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
+  isUser() {
+    var currentUser = FirebaseAuth.instance.currentUser;
+
+    if (currentUser == null) return false;
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,6 +44,6 @@ class App extends StatelessWidget {
         supportedLocales: context.supportedLocales,
         locale: context.locale,
         debugShowCheckedModeBanner: false,
-        home: Login());
+        home: isUser() ? MyApp() : Login());
   }
 }
